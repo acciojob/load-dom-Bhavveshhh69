acb37de04d2d098d51e965f23e7dc16d837084e6
Bhavesh Suresh <149222281+Bhavveshhh69@users.noreply.github.com>
2025-12-05 21:52:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+window.addEventListener('DOMContentLoaded', () => {
+  // Write the required string to the page when DOM is loaded
+  document.body.textContent = "DOM load success";
+});
